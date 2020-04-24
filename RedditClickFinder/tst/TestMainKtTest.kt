e1d538ac90d0159952270848819763e5b859961b
cshinaver <bshinaver@gmail.com>
2020-04-23 20:32:12 -0400

    Working kotlin main and test function

---
+++ RedditClickFinder/tst/TestMainKtTest.kt
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class TestMainKtTest {
+
+    @Test
+    fun main() {
+        assertEquals(5, 5)
+    }
+}
